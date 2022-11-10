af085d3ec29f26c86f50bec5b92776d1cb73afe0
KarlosElyas <karloselyas2@gmail.com>
2022-11-10 13:26:28 -0300

    Service layer, component registration

--- src/main/java/com/educandoaweb/coursespring/resources/UserResource.java
+++ src/main/java/com/educandoaweb/coursespring/resources/UserResource.java
@@ -1,18 +1,33 @@
 package com.educandoaweb.coursespring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoaweb.coursespring.entities.User;
+import com.educandoaweb.coursespring.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users") // definido o endereco http do tipo GET
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping // requisicao tipo get (get/post)
-	public ResponseEntity<User> lookforAll(){
-		return ResponseEntity.ok().body(new User(1L, "Maria", "maria@gmail", "009877", "amodsv"));
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll(); // e chamado a service que por sua vez chama a repository
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){ // permite pegar o id da url
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
